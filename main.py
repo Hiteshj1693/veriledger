0edc2e9fdd5f4189d1db1df59caf04367a11df06
Hitesh Jethava <Hiteshjethava161611@gmail.com>
2025-12-25 23:40:03 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World!!"}
+
+@app.get("/root-path")
+async def read_root_path():
+    return {"message": "Hello, Root Path!!"}
